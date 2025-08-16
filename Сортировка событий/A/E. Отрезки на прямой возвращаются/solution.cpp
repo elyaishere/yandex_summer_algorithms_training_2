f5f0d24e9dd4bfe939430fe159733ddaaa4535a4
Olga Gorbunova <olya-const@yandex-team.ru>
2025-08-16 17:17:35 +0300

    add readme

---
+++ Сортировка событий/A/E. Отрезки на прямой возвращаются/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n, k;
+    std::cin >> n >> k;
+    
+    return 0;
+}
